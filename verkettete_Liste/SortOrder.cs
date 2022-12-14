b10bb0775690ce82a5fe80cb078c492510b5e83d
RavenTamer <jeremias.furrer@hotmail.com>
2022-12-14 17:02:09 +0100

    Neueste Version

--- verkettete_Liste/SortOrder.cs
+++ verkettete_Liste/SortOrder.cs
@@ -8,10 +8,24 @@ namespace verkettete_Liste
 {
     internal class SortOrder
     {
-        public enum sortorder
+        Zahlenspeicher zs = new();
+        public void Ascending()
         {
-            ASCENDING,
-            DESCENDING
+            int smallest = 0;
+            string[] zahlenspeicher = zs.Get();
+            for(int i = 0; i<zahlenspeicher.Length; i++)
+            {
+                if(Convert.ToInt32(zahlenspeicher[i]) < smallest)
+                {
+                    smallest = Convert.ToInt32(zahlenspeicher[i]);
+                }
+
+            }
+        }
+        public void Descending()
+        {
+
         }
     }
 }
+
